fd04bbc13c5695f348f06ddab5bd19f5ff37f9f6
NotNormJustice <nareinvignesh27@proton.me>
2024-06-14 19:07:22 -0500

    moving files

---
+++ Scripts/Camera.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Camera : Camera2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("ui_accept"))
+		{
+			Tween tween = CreateTween();
+			tween.TweenProperty(this, "position.x", -2000, 1.0f).AsRelative();
+			tween.Play();
+		}
+	}
+}
