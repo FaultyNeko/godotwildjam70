542948a0fa61dee3c1e862b7a2521d37ad663425
NotNormJustice <nareinvignesh27@proton.me>
2024-06-19 00:57:51 -0500

    Ranged Enemy

---
+++ Scripts/RangedEnemy.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+using System.Numerics;
+using Vector2 = Godot.Vector2;
+
+public partial class RangedEnemy : CharacterBody2D
+{
+	private Player _player;
+	private PackedScene _arrow;
+	private Tween _shoot;
+	private Vector2 _position;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_player = GetNode<Player>("%Knight");
+		_arrow = GD.Load<PackedScene>("res://Scenes/Arrow.tscn");
+		_shoot = CreateTween();
+		_shoot.TweenInterval(2);
+		_shoot.TweenCallback(Callable.From(() => _position = _player.GlobalPosition));
+		_shoot.TweenCallback(Callable.From(() => Modulate = Colors.Red));
+		_shoot.TweenInterval(1);
+		_shoot.TweenCallback(Callable.From(Shoot));
+		_shoot.SetLoops();
+		_shoot.Play();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private void Shoot()
+	{
+		// Should not shoot if player is too far
+		if (_position.DistanceTo(Position) > 1000)
+			return;
+		Area2D arrow = _arrow.Instantiate<Area2D>();
+		AddChild(arrow);
+		arrow.Rotation = Position.AngleToPoint(_position);
+		Tween arrowTween = arrow.CreateTween();
+		arrowTween.TweenProperty(arrow, "position", ToLocal(_position), .5);
+		arrowTween.TweenCallback(Callable.From(arrow.QueueFree));
+		arrowTween.Play();
+		Modulate = Colors.White;
+	}
+	
+}
