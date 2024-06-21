105ebbc4d18a7d0273606425e1627d0595812894
Andres Vega <thefaultyjetpack@gmail.com>
2024-06-21 14:16:23 -0400

    fixing unintentional bug >.>

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -7,10 +7,10 @@ public partial class GameManager : Node
 	{
 		// Assuming you have nodes named Tutorial1Instance1, Tutorial1Instance2, etc.
 		var tutorial1Instance1 = GetNode<tutorialtriggers>("Tutorial1");
-		tutorial1Instance1.SetTutorialText("Press jump while touching\n a wall to perform a wall jump.");
+		tutorial1Instance1.SetTutorialText("Press jump while touching\na wall to perform a wall jump.");
 
-		//var tutorial1Instance2 = GetNode<tutorialtriggers>("Tutorial1Instance2");
-		//tutorial1Instance2.SetTutorialText("Use arrow keys to move left or right.");
+		var tutorial1Instance2 = GetNode<tutorialtriggers>("Tutorial2");
+		tutorial1Instance2.SetTutorialText("You got 2 types of attacks\ntry out your melee and ranged one.");
 
 		// Add more instances as needed
 	}
--- Scripts/tutorialtriggers.cs
+++ Scripts/tutorialtriggers.cs
@@ -3,45 +3,45 @@ using System;
 
 public partial class tutorialtriggers : Area2D
 {
-	private Popup _popupInstance;
+	private Popup popupInstance;
 
 	public override void _Ready()
 	{
-		// Locate the popup node within the parent's children and store the reference
-		_popupInstance = GetParent().GetNode<Popup>("Tutorialpopup");
-		if (_popupInstance != null)
-		{
-			_popupInstance.Visible = false; // Initially hidden
-		}
-
-		// Connect the body_entered and body_exited signals to their respective methods
+		popupInstance = GetNode<Popup>("Tutorialpopup");
+		popupInstance.Visible = false; // Initially hidden
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
 		Connect("body_exited", new Callable(this, nameof(OnBodyExited)));
 	}
 
-	// Method to set the tutorial text on the popup
 	public void SetTutorialText(string text)
 	{
-		_popupInstance?.SetText(text);
-	}
-
-	// Called when a body enters the area
-	private void OnBodyEntered(Node body)
-	{
-		// Check if the body is on layer 1 and make the popup visible
-		if (body is PhysicsBody2D physicsBody && (physicsBody.CollisionLayer & 1) != 0)
+		if (popupInstance != null)
 		{
-			_popupInstance.Visible = true;
+			popupInstance.SetText(text);
 		}
 	}
 
-	// Called when a body exits the area
-	private void OnBodyExited(Node body)
+	private void OnBodyEntered(Node body)
 	{
-		// Check if the body is on layer 1 and hide the popup
+		// Check if the body is on layer 1
 		if (body is PhysicsBody2D physicsBody && (physicsBody.CollisionLayer & 1) != 0)
 		{
-			_popupInstance.Visible = false;
+			if (popupInstance != null)
+			{
+				popupInstance.Visible = true; // Show the popup
+			}
+		}
+	}
+
+	private void OnBodyExited(Node body)
+	{
+		// Check if the body is on layer 1
+		if (body is PhysicsBody2D physicsBody && (physicsBody.CollisionLayer & 1) != 0)
+		{
+			if (popupInstance != null)
+			{
+				popupInstance.Visible = false; // Hide the popup
+			}
 		}
 	}
 }
