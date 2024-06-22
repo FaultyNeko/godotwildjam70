4427ff8ef0adeb4f8c739fa47adcbfaf9a07d11d
Rayhan <rayhanranjith@gmail.com>
2024-06-22 13:02:11 -0500

    Integrated the music and fixed start screen

--- Scripts/MusicPlayer.cs
+++ Scripts/MusicPlayer.cs
@@ -36,9 +36,9 @@ public partial class MusicPlayer : Node
 		using var file = FileAccess.Open(PATH_TO_TRACKLIST, FileAccess.ModeFlags.Read);
 		string json = file.GetAsText();
 
-		GD.Print(json);
+		//GD.Print(json);
 		var tl = JsonConvert.DeserializeObject<Tracklist>(json);
-		GD.Print(tl.tracks.Count);
+		//GD.Print(tl.tracks.Count);
 
 		// Create the nodes for each AudioStreamPlayer
 		foreach (Track t in tl.tracks) {
@@ -77,7 +77,7 @@ public partial class MusicPlayer : Node
 	{
 		Node curr = GetNode($"{CurrentSongPath}");
 		if (IsPlaying(curr)) {
-			GD.Print("Stopping current track...");
+			//GD.Print("Stopping current track...");
 			StopTrack(curr);
 		}
 
@@ -111,8 +111,9 @@ public partial class MusicPlayer : Node
 	}
 
 	private void StopTrack(Node n) {
-		if (n is AudioStreamPlayer) {
-			((AudioStreamPlayer)n).Stop();
+		if (n is AudioStreamPlayer)
+		{
+			((AudioStreamPlayer)n).StreamPaused = true;
 		}
 		if (n is AudioStreamLayered) {
 			((AudioStreamLayered)n).Stop();
--- Scripts/StartCutscene.cs
+++ Scripts/StartCutscene.cs
@@ -8,19 +8,22 @@ public partial class StartCutscene : Control
 	{
 		TextureRect background = GetNode<TextureRect>("TextureRect");
 		Tween fadeIn = background.CreateTween();
-		fadeIn.TweenProperty(background, "modulate:a", 1, 3);
+		fadeIn.TweenProperty(background, "modulate:a", 1, 2);
 		fadeIn.Finished += () => FadeInLabels();
 		fadeIn.Play();
 	}
 
 	public void FadeInLabels()
 	{
-		Label label = (Label)GetTree().GetFirstNodeInGroup("StartCutsceneLabels");
-		Tween labelTween = label.CreateTween();
-		labelTween.TweenProperty(label, "modulate:a", 1, 3);
-		labelTween.Finished += () => FadeInLabels();
-		label.RemoveFromGroup("StartCutsceneLabels");
-		labelTween.Play();
+		if (GetTree().GetFirstNodeInGroup("StartCutsceneLabels") != null)
+		{
+			Label label = (Label)GetTree().GetFirstNodeInGroup("StartCutsceneLabels");
+			Tween labelTween = label.CreateTween();
+			labelTween.TweenProperty(label, "modulate:a", 1, 2);
+			labelTween.Finished += () => FadeInLabels();
+			label.RemoveFromGroup("StartCutsceneLabels");
+			labelTween.Play();
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
